2f6aec57179591d26ea56a87a9d52ebbaadfd2c2
marckie419 <55561884+marckie419@users.noreply.github.com>
2024-07-02 16:49:54 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Snapping behavior
-    let sections = document.querySelectorAll('main section');
-    let main = document.querySelector('main');
+    // Remove snapping behavior
+    // let sections = document.querySelectorAll('main section');
+    // let main = document.querySelector('main');
 
-    if (window.innerWidth > 600) {
-        main.addEventListener('scroll', () => {
-            let closestSection = null;
-            let minDistance = Infinity;
+    // if (window.innerWidth > 600) {
+    //     main.addEventListener('scroll', () => {
+    //         let closestSection = null;
+    //         let minDistance = Infinity;
 
-            sections.forEach(section => {
-                let rect = section.getBoundingClientRect();
-                let distance = Math.abs(rect.top);
+    //         sections.forEach(section => {
+    //             let rect = section.getBoundingClientRect();
+    //             let distance = Math.abs(rect.top);
 
-                if (distance < minDistance) {
-                    minDistance = distance;
-                    closestSection = section;
-                }
-            });
+    //             if (distance < minDistance) {
+    //                 minDistance = distance;
+    //                 closestSection = section;
+    //             }
+    //         });
 
-            if (closestSection) {
-                closestSection.scrollIntoView({ behavior: 'smooth' });
-            }
-        });
-    }
-
+    //         // Disable smooth scrolling for testing
+    //         // if (closestSection) {
+    //         //    closestSection.scrollIntoView({ behavior: 'smooth' });
+    //         // }
+    //     });
+    // }
     // Existing code for splitview
     var parent = document.querySelector('.splitview'),
         topPanel = parent.querySelector('.top'),
@@ -55,17 +55,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const items = document.querySelectorAll(".accordion button");
 
-function toggleAccordion() {
-  const itemToggle = this.getAttribute('aria-expanded');
-  
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
-  }
-  
-  if (itemToggle == 'false') {
-    this.setAttribute('aria-expanded', 'true');
-  }
-}
+    function toggleAccordion() {
+        const itemToggle = this.getAttribute('aria-expanded');
+        
+        for (i = 0; i < items.length; i++) {
+            items[i].setAttribute('aria-expanded', 'false');
+        }
+        
+        if (itemToggle == 'false') {
+            this.setAttribute('aria-expanded', 'true');
+        }
+    }
 
-items.forEach(item => item.addEventListener('click', toggleAccordion));
+    items.forEach(item => item.addEventListener('click', toggleAccordion));
 });
