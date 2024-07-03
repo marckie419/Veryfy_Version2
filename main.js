9c94c8bdfc5d5ffc67d0ae2ef9d8e08413d986c1
marckie419 <55561884+marckie419@users.noreply.github.com>
2024-07-03 11:43:30 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -68,4 +68,67 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     items.forEach(item => item.addEventListener('click', toggleAccordion));
-});
+    
+
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+class Carousel {
+  constructor(container, items, controls) {
+    this.carouselContainer = container;
+    this.carouselControls = controls;
+    this.carouselArray = [...items];
+  }
+
+  updateGallery() {
+    this.carouselArray.forEach(el => {
+      el.classList.remove('gallery-item-1', 'gallery-item-2', 'gallery-item-3', 'gallery-item-4', 'gallery-item-5', 'gallery-item-6');
+    });
+    this.carouselArray.slice(0, 6).forEach((el, i) => {
+      el.classList.add(`gallery-item-${i + 1}`);
+    });
+  }
+
+  setCurrentState(direction) {
+    if (direction.className.includes('gallery-controls-previous')) {
+      this.carouselArray.unshift(this.carouselArray.pop());
+    } else {
+      this.carouselArray.push(this.carouselArray.shift());
+    }
+    this.updateGallery();
+  }
+
+  setControls() {
+    this.carouselControls.forEach(control => {
+      const button = document.createElement('button');
+      button.className = `gallery-controls-${control}`;
+      button.innerText = control;
+      galleryControlsContainer.appendChild(button);
+    });
+  }
+
+  useControls() {
+    const triggers = [...galleryControlsContainer.childNodes];
+    triggers.forEach(control => {
+      control.addEventListener('click', e => {
+        e.preventDefault();
+        this.setCurrentState(control);
+      });
+    });
+  }
+}
+
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
+
+exampleCarousel.setControls();
+exampleCarousel.useControls();
+
+
+  window.addEventListener('load', navmenuScrollspy);
+  document.addEventListener('scroll', navmenuScrollspy);
+
+  
+
+})();
